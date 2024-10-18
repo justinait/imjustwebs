df59a901194d31701698ed19eb11c9d8e2a7e857
justinait <justiiturraspe@gmail.com>
2024-10-18 20:20:49 -0300

    projects

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
-        <Link to='/'>I'm Just Webs</Link>
+    <div style={{textAlign:'center'}}>
+      <Link to='/' className='imjustwebsNavbar'>I'm Just Webs</Link>
     </div>
   )
 }
