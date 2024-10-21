a53da91723eae3b5c686bc3092ba161dae85dec7
justinait <justiiturraspe@gmail.com>
2024-10-21 12:35:51 -0300

    webs

--- src/Components/Freelance/Freelance.jsx
+++ src/Components/Freelance/Freelance.jsx
@@ -14,10 +14,9 @@ import regia from '../../assets/webs/regia.png';
 import './Freelance.css';
 
 function Freelance() {
-    const row1Ref = useRef(null);
-    const row2Ref = useRef(null);
-    const [isPaused, setIsPaused] = useState(false);
-    let scrollValue = 0; // Mantener el estado del desplazamiento
+
+    const [iframeUrl, setIframeUrl] = useState("");
+    const [isVisible, setIsVisible] = useState(false);
   
     const projects = [
       { name: 'E2 Yacht Services', image: yacht, web: 'https://www.e2yachtservices.com' },
@@ -33,68 +32,36 @@ function Freelance() {
       { name: 'Jorgelina Parkinson', image: jorgelina, web: 'https://www.jorgelinaparkinson.com/' },
       { name: 'Regia', image: regia, web: 'https://www.regia.ar/' }
     ];
-  
-    const firstHalf = projects.slice(0, Math.ceil(projects.length / 2));
-    const secondHalf = projects.slice(Math.ceil(projects.length / 2)).reverse();
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        if (!isPaused) {
-          scrollValue += 2; // Incrementa el valor de desplazamiento
-  
-          if (row1Ref.current && row2Ref.current) {
-            const row1MaxScroll = row1Ref.current.scrollWidth - row1Ref.current.clientWidth;
-            const row2MaxScroll = row2Ref.current.scrollWidth - row2Ref.current.clientWidth;
-  
-            // Desplaza la fila 1 hacia la derecha
-            row1Ref.current.scrollLeft = scrollValue;
-  
-            // Desplaza la fila 2 hacia la izquierda
-            row2Ref.current.scrollLeft = row2MaxScroll - scrollValue;
-  
-            // Reinicia el desplazamiento si se alcanza el final
-            if (scrollValue >= row1MaxScroll) {
-              scrollValue = 0;
-            }
-          }
-        }
-      }, 50); // Ajusta la velocidad del desplazamiento cambiando el intervalo
-  
-      const pauseScrolling = () => setIsPaused(true);
-      const resumeScrolling = () => setIsPaused(false);
-  
-      window.addEventListener('touchstart', pauseScrolling);
-      window.addEventListener('touchend', resumeScrolling);
-      window.addEventListener('wheel', pauseScrolling);
-      window.addEventListener('scroll', pauseScrolling);
-      
-      return () => {
-        clearInterval(interval);
-        window.removeEventListener('touchstart', pauseScrolling);
-        window.removeEventListener('touchend', resumeScrolling);
-        window.removeEventListener('wheel', pauseScrolling);
-        window.removeEventListener('scroll', pauseScrolling);
-      };
-    }, [isPaused]);
+    const openIframe = (url) => {
+        setIframeUrl(url);
+        setIsVisible(true);
+    };
+    
+    const closeIframe = () => {
+        setIsVisible(false);
+        setTimeout(() => setIframeUrl(""), 500); // Limpiar el iframe después de la animación
+    };
   
     return (
-      <div className="projectScrollContainer">
-        <div className="projectRow" ref={row1Ref}>
-          {firstHalf.map((e, i) => (
-            <div target="_blank" rel="noreferrer" key={i} className="projectContainer">
-              <img src={e.image} alt={e.name} className="projectMockup" />
-              <a href={e.web} className="projectText">{e.name}</a>
-            </div>
-          ))}
+      <div className="mockup-container">
+        {projects.map((e, i) => (
+        <img
+          key={i}
+          src={e.image}
+          alt={`Proyecto ${e.name}`}
+          className="mockup"
+          onClick={() => openIframe(e.web)}
+        />
+      ))}
+
+      {isVisible && (
+        <div id="iframe-container" className={`iframe-container ${isVisible ? "visible" : ""}`}>
+          <iframe id="project-iframe" src={iframeUrl} frameBorder="0"></iframe>
+          <button id="close-iframe" onClick={closeIframe}>
+            Cerrar
+          </button>
         </div>
-        <div className="projectRow reverse" ref={row2Ref}>
-          {secondHalf.map((e, i) => (
-            <div target="_blank" rel="noreferrer" key={i} className="projectContainer">
-              <img src={e.image} alt={e.name} className="projectMockup" />
-              <a href={e.web} className="projectText">{e.name}</a>
-            </div>
-          ))}
-        </div>
+      )}
       </div>
     );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,6 +6,14 @@ function Home() {
   return (
     <>
       <Navbar/>
+      <p>
+        Desde Argentina, me dedico a desarrollar portfolios personalizados, páginas corporativas y 
+        landing pages que combinan diseño visual y funcionalidad. Trabajo junto a expertos en diseño 
+        para crear soluciones web modernas que ayudan a negocios y profesionales de todo el mundo a destacarse.
+        <br /><br />
+        Con un enfoque en el frontend, uso tecnologías como React JS para desarrollar sitios web 
+        interactivos y adaptables, asegurando una experiencia de usuario fluida y atractiva.
+      </p>
       <Freelance />
     </>
   )
