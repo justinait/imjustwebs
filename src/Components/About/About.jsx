45fbce9953c1b5c31fc3091593db543386914a11
justinait <justiiturraspe@gmail.com>
2024-10-22 12:26:41 -0300

    freelance

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './About.css'
+
+function About() {
+  return (
+    <div>
+        
+      <p>
+        Desde Argentina, me dedico a desarrollar portfolios personalizados, páginas corporativas y 
+        landing pages que combinan diseño visual y funcionalidad. Trabajo junto a expertos en diseño 
+        para crear soluciones web modernas que ayudan a negocios y profesionales de todo el mundo a destacarse.
+        <br /><br />
+        Con un enfoque en el frontend, uso tecnologías como React JS para desarrollar sitios web 
+        interactivos y adaptables, asegurando una experiencia de usuario fluida y atractiva.
+      </p>
+    </div>
+  )
+}
+
+export default About
