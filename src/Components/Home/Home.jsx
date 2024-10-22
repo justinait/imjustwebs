0151fa4b4fa6ad140623a9a17363b728c4705e1f
justinait <justiiturraspe@gmail.com>
2024-10-22 12:32:07 -0300

    home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Freelance from '../Freelance/Freelance'
+import './Home.css'
 
 function Home() {
   return (
-    <>
+    <div className='homeContainer'>
       <Navbar/>
+      <div className='homeBoxText'>
+        <p className='homeText'> <strong>Explorá algunos de mis proyectos destacados</strong></p>
+        <p className='homeText'>Hacé clic y navega por la página.</p>
+      </div>
       <Freelance />
-    </>
+    </div>
   )
 }
 
