d1f2b61598fd5e29cf82d8accc86230c2ef4d3f6
justinait <justiiturraspe@gmail.com>
2024-10-22 16:26:25 -0300

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,8 +4,13 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <div style={{textAlign:'center'}}>
+    <div className='navbarContainer'>
       <Link to='/' className='imjustwebsNavbar'>I'm Just Webs</Link>
+      
+      <div className='navbarLinks'>
+        <Link to='/contact'>CONTACTO</Link>
+        <Link to='/home'>PROYECTOS</Link>
+      </div>
     </div>
   )
 }
