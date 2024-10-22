fee03d4814282ea13bef165d58bf1aa3c4112233
justinait <justiiturraspe@gmail.com>
2024-10-22 17:32:31 -0300

    form

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,17 +1,106 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import './About.css'
 import Navbar from '../Navbar/Navbar'
-
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import emailjs from '@emailjs/browser';
 
 function About() {
+
+  const {
+    VITE_API_SERVICEID: service_id,
+    VITE_API_TEMPLATEID: template_id,
+    VITE_API_PUBLICID: public_id,
+  } = import.meta.env;
+
+  const form = useRef();
+  const [btnsubmit, setBtnsubmit] = useState(true);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [input, setInput] = useState({
+    user_name: { value: '', error: '' },
+    user_email: { value: '', error: '' },
+    user_phone: { value: '', error: '' },
+    message: { value: '', error: '' },
+  });
+
+  const handleInputChange = (e) => {
+    setInput((prev) => ({
+      ...prev,
+      [e.target.name]: {
+        value: e.target.value,
+        error: null,
+      },
+    }));
+  };
+
+  const clearInputValue = () => {
+    setInput({
+      user_name: { value: '', error: '' },
+      user_email: { value: '', error: '' },
+      user_phone: { value: '', error: '' },
+      message: { value: '', error: '' },
+    });
+  };
+
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => {
+        setShow(false);
+      }, 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [show]);
+
+  const sendEmailForm = (e) => {
+    e.preventDefault();
+    let stop = false;
+    Object.keys(input).forEach((key) => {
+      if (input[key].value.trim().length === 0) {
+        stop = true;
+        setInput((prev) => ({
+          ...prev,
+          [key]: {
+            ...prev[key],
+            error: '*Este campo es obligatorio',
+          },
+        }));
+      }
+    });
+    if (stop) return;
+
+    setBtnsubmit(true);
+    handleShow();
+
+    emailjs.sendForm(service_id, template_id, form.current, public_id).then(
+      (result) => {
+        console.log(result.text);
+        clearInputValue();
+        setBtnsubmit(false);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
   return (
-    <div className='homeContainer'>
+    <div className=''>
       <Navbar/>
+      <p className='aboutText'>
+        Desde Argentina, me dedico a desarrollar portfolios personalizados, páginas corporativas y 
+        landing pages que combinan diseño visual y funcionalidad. Trabajo junto a expertos en diseño 
+        para crear soluciones web modernas que ayudan a negocios y profesionales de todo el mundo a destacarse.
+        <br /><br />
+        Con un enfoque en el frontend, uso tecnologías como React JS para desarrollar sitios web 
+        interactivos y adaptables, asegurando una experiencia de usuario fluida y atractiva.
+      </p>
+
       <div className='socialMedia'>
+        <p>Encontrame en:</p>
         <a href='https://github.com/justinait' target="_blank">
           <GitHubIcon className='socialMediaIcon' fontSize='large' />
         </a>
@@ -26,17 +115,27 @@ function About() {
           fontSize='large'
         />
       
-        
-    </div>
-      <p className='aboutText'>
-        Desde Argentina, me dedico a desarrollar portfolios personalizados, páginas corporativas y 
-        landing pages que combinan diseño visual y funcionalidad. Trabajo junto a expertos en diseño 
-        para crear soluciones web modernas que ayudan a negocios y profesionales de todo el mundo a destacarse.
-        <br /><br />
-        Con un enfoque en el frontend, uso tecnologías como React JS para desarrollar sitios web 
-        interactivos y adaptables, asegurando una experiencia de usuario fluida y atractiva.
-      </p>
+      </div>
       
+      <div>
+        <h4>¿Tenés un proyecto en mente? <strong> Hablemos</strong></h4>
+
+        <form ref={form} onSubmit={sendEmailForm} className='formContainer'>
+        
+        <input type="text" name="user_name" placeholder='Nombre y apellido'  value={input['user_name'] ? input['user_name'].value : ''}  className='inputForm' onChange={handleInputChange} />
+        
+        <input type="email" name="user_email" placeholder='Email' value={input['user_email'] ? input['user_email'].value : ''} className='inputForm' onChange={handleInputChange} />
+
+        <input type="number" name="user_phone" placeholder='Teléfono / Celular' value={input['user_phone'] ? input['user_phone'].value : ''} className='inputForm' onChange={handleInputChange} />
+        
+        <textarea name="message" placeholder='Mensaje' value={input['message'] ? input['message'].value : ''} className='inputForm messageForm'  onChange={handleInputChange}/>
+        
+        <div>
+          <input type="submit" value="Enviar" className='formSendButton'/>
+        </div>
+
+      </form>
+      </div>
     </div>
   )
 }
